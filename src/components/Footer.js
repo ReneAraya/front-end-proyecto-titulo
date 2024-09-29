6f2625c881326f73156a16917ed70b836487582e
René Araya <renepatricioarayarojas@gmail.com>
2024-09-29 14:27:36 -0300

    funcionando basicamente

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+// src/components/Footer.js
+
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>&copy; {new Date().getFullYear()} Tu Empresa. Todos los derechos reservados.</p>
+        <ul className="social-links">
+          <li><a href="https://facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a></li>
+          <li><a href="https://twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a></li>
+          <li><a href="https://instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a></li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
