60e82c3d2e09c402e35ad058fdddb095af05a5af
René Araya <renepatricioarayarojas@gmail.com>
2024-09-30 21:05:49 -0300

    error anidacion arreglado

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Main from './components/Main';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Main />
-        <Footer />
-      </div>
-    </Router>
+    <div className="App">
+      <Navbar />
+      <Main />
+      <Footer />
+    </div>
   );
 }
 
